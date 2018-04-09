9fbb593ea7d469a2cb4ccde92f70260dd0fda6b4
SulaimanAloraini <37535162+SulaimanAloraini@users.noreply.github.com>
2018-04-09 15:06:38 +0300

    Create Repsonse Method

--- Shared/Response.ts
+++ Shared/Response.ts
@@ -1,1 +1,7 @@
 // To send response (Error or Success)
+
+const Repsonse = (response:any, Status:number, Message:string, Data:T) => {
+  response
+    .StatusCode(Status)
+    .json({ Message, Data })
+}
